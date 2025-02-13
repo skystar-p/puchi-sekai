d5c0e5765f9f0ef6cae392d9e85f2df9c728d101
Jaehyeon Park <skystar@skystar.dev>
2025-02-13 23:16:11 +0900

    default model path

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -2,5 +2,15 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
+    #[serde(default = "default_initial_model_data_path")]
     pub initial_model_data_path: String,
+}
+
+fn default_initial_model_data_path() -> String {
+    std::env::current_dir()
+        .unwrap()
+        .join("model.zip")
+        .to_str()
+        .unwrap()
+        .to_string()
 }
