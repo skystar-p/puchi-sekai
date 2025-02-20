496f35b7bd0f4bd962076c680acfff1d036a9e86
Jaehyeon Park <skystar@skystar.dev>
2025-02-20 22:58:51 +0900

    fix package-ipc

--- package-ipc.nix
+++ package-ipc.nix
@@ -3,15 +3,24 @@
 ,
 }:
 
-
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "puchi-sekai-ipc";
   version = "0.0.1";
 
   src = lib.cleanSource ./.;
 
   buildAndTestSubdir = "ipc";
+  cargoRoot = "ipc";
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-/srWdDfwawiQz90W5r2GhmqHeRJvbmVAjZp0QhJRsyM=";
+
+  cargoDeps = rustPlatform.fetchCargoVendor {
+    inherit
+      pname
+      version
+      src
+      cargoRoot
+      ;
+    hash = "sha256-rsf9xLmkLRo4JvZDb4PGWrjYHkUg/wjlG9kgx0Ee1nM=";
+  };
 }
